5bdb1a9f7f473161e67b68a4684f3510a68c26d6
moises <moisesmariner@gmail.com>
2025-11-07 21:22:41 -0500

    Subir mapa del juego

---
+++ Juego.py
@@ -0,0 +1,44 @@
+from Mapa import *
+from colorama import Fore, Back
+
+mapa = Mapa()
+
+# funcion utilitaria para obtener el numero correspondiente
+# a una direccion a partir de un string. Si no es valido,
+# retorna -1
+def obtenerNumDireccion(string):
+  if string == "arriba":    return ARR
+  if string == "abajo":     return ABJ
+  if string == "izquierda": return IZQ
+  if string == "derecha":   return DER
+  return -1
+
+def mover(direc):
+  mapa.actual= mapa.actual.conex[direc]
+
+while True:
+  print(f"\n{Fore.GREEN}Estas actualmente en {mapa.actual.nombre}")
+  print(Fore.RESET)  
+
+  arriba = mapa.actual.conex[ARR]
+  abajo = mapa.actual.conex[ABJ]
+  izquier = mapa.actual.conex[IZQ]
+  derecha = mapa.actual.conex[DER]
+
+  print("Hacia donde te deseas mover?")
+  print(Fore.RESET + " - Arriba:   ", Fore.GREEN + arriba.nombre if arriba is not None else Fore.RED + "NADA")
+  print(Fore.RESET + " - Abajo:    ", Fore.GREEN + abajo.nombre if abajo is not None else Fore.RED + "NADA")
+  print(Fore.RESET + " - Izquierda:", Fore.GREEN + izquier.nombre if izquier is not None else Fore.RED + "NADA")
+  print(Fore.RESET + " - Derecha:  ", Fore.GREEN + derecha.nombre if derecha is not None else Fore.RED + "NADA")
+  print(Back.WHITE + Fore.BLUE)
+  string = input("--> ")
+  direc = obtenerNumDireccion(string)
+  print(Back.RESET)
+  if direc == -1:
+    print(f"{Fore.RED}Direccion no valida{Fore.RESET}")
+    continue
+  
+  if mapa.actual.conex[direc] is not None:
+    mover(direc)
+  else:
+    print(F"{Fore.RED}No hay nada en esa direccion{Fore.RESET}")
---
+++ Mapa.py
@@ -0,0 +1,73 @@
+# "Ubicación" representa un punto especifico del mapa. Tiene
+# un nombre y un arreglo de 4 elementos con las conexiones
+# a otras ubicaciones, ordenadas así:
+# conex[0] : izquierda
+# conex[1] : derecha
+# conex[2] : arriba
+# conex[3] : abajo
+class Ubicacion:
+  def __init__(self, nombre):
+    self.nombre = nombre
+    self.conex = [None] * 4
+
+# Constantes de direccion
+IZQ = 0
+DER = 1
+ARR = 2
+ABJ = 3
+
+class Mapa:
+  def __init__(self):
+    # Crear los lugares que componen el mapa
+    aldea = Ubicacion("Aldea principal")
+    lago = Ubicacion("Lago sagrado")
+    campamento = Ubicacion("Campamento")
+    mazmorra = Ubicacion("Mazmorra")
+    castillo = Ubicacion("Castillo antiguo")
+    bosque = Ubicacion("Bosque encantado")
+
+    # Crear las rutas que conectan los lugares
+    ruta1 = Ubicacion("Ruta 1")
+    ruta2 = Ubicacion("Ruta 2")
+    ruta3 = Ubicacion("Ruta 3")
+    ruta4 = Ubicacion("Ruta 4")
+    ruta5 = Ubicacion("Ruta 5")
+    ruta6 = Ubicacion("Ruta 6")
+    
+    # Establecer sus conexiones bidireccionales
+    self.agregarconex(aldea, ABJ, ruta1)
+    self.agregarconex(ruta1, DER, lago)
+    self.agregarconex(aldea, DER, ruta2)
+    self.agregarconex(ruta2, DER, campamento)
+    self.agregarconex(campamento, DER, ruta3)
+    self.agregarconex(ruta3, DER, ruta4)
+    self.agregarconex(ruta4, ABJ, mazmorra)
+    self.agregarconex(ruta4, ARR, castillo)
+    self.agregarconex(castillo, IZQ, ruta6)
+    self.agregarconex(ruta6, IZQ, bosque)
+    self.agregarconex(ruta6, ABJ, ruta5)
+    self.agregarconex(ruta5, ABJ, campamento)
+
+    # Establecer la ubicacion actual inicial
+    self.actual = aldea
+
+
+# El siguiente metodo añade una conexion de forma bidireccional
+# desde U1 en la direccion "direc" hacia U2. Es decir, que
+# U2 tambien hara referencia, en la direccion contraria, a U1
+
+  def agregarconex(self, U1, direc, U2):
+    U1.conex[direc] = U2
+    direc2 = -1
+
+    match direc:
+      case 0:
+        direc2 = DER
+      case 1:
+        direc2 = IZQ
+      case 2:
+        direc2 = ABJ
+      case 3:
+        direc2 = ARR
+
+    U2.conex[direc2] = U1
